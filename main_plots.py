c6cdc8a7755b36bc946bf476011fbb213b7fc4a6
Marie POINT <marie.point@hotmail.com>
2025-03-28 00:38:23 +0100

    plots

--- main_plots.py
+++ main_plots.py
@@ -13,7 +13,7 @@ def stats_bot_bot(n_parties=1000, nb_val=100, taille_pyramide=27, nb_cartes_par_
 
     j1_wins = 0; ia_wins = 0
     for  _ in range(n_parties) :
-        cards = [Carte(valeur, couleur, custom_val=True) for valeur in range(1, nb_val) for couleur in Carte.COULEURS]  # 4 cartes par valeur
+        cards = [Carte(valeur, couleur, custom_val=True) for valeur in range(1, nb_val+1) for couleur in Carte.COULEURS]  # 4 cartes par valeur
         custom_deck = Deck(custom_cards=cards)
 
         j1 = JoueurPresqueRandom("Bertrand")
@@ -39,7 +39,7 @@ def stats_bot_ia(n_parties=1000, nb_val=100, taille_pyramide=27, nb_cartes_par_j
 
     j1_wins = 0; ia_wins = 0
     for  _ in range(n_parties) :
-        cards = [Carte(valeur, couleur, custom_val=True) for valeur in range(1, nb_val) for couleur in Carte.COULEURS]  # 4 cartes par valeur
+        cards = [Carte(valeur, couleur, custom_val=True) for valeur in range(1, nb_val+1) for couleur in Carte.COULEURS]  # 4 cartes par valeur
         custom_deck = Deck(custom_cards=cards)
 
         j1 = JoueurPresqueRandom("Bertrand")
@@ -65,7 +65,7 @@ def stats_rand_ia(n_parties=1000, nb_val=100, taille_pyramide=27, nb_cartes_par_
 
     j1_wins = 0; ia_wins = 0
     for  _ in range(n_parties) :
-        cards = [Carte(valeur, couleur, custom_val=True) for valeur in range(1, nb_val) for couleur in Carte.COULEURS]  # 4 cartes par valeur
+        cards = [Carte(valeur, couleur, custom_val=True) for valeur in range(1, nb_val+1) for couleur in Carte.COULEURS]  # 4 cartes par valeur
         custom_deck = Deck(custom_cards=cards)
 
         j1 = JoueurRandom("Bertrand")
@@ -87,6 +87,31 @@ def stats_rand_ia(n_parties=1000, nb_val=100, taille_pyramide=27, nb_cartes_par_
     return j1_wins, ia_wins, n_parties, j1, j2
 
 
+def stats_ia_ia(n_parties=1000, nb_val=100, taille_pyramide=27, nb_cartes_par_joueur=4, afficher_score=False, ui=False) :
+
+    j1_wins = 0; ia_wins = 0
+    for  _ in range(n_parties) :
+        cards = [Carte(valeur, couleur, custom_val=True) for valeur in range(1, nb_val+1) for couleur in Carte.COULEURS]  # 4 cartes par valeur
+        custom_deck = Deck(custom_cards=cards)
+
+        j1 = AdversaireIA("Igor")
+        j2 = AdversaireIA("Isabelle")
+
+        game = Game(joueurs=[j1, j2], custom_deck=custom_deck, taille_pyramide=taille_pyramide, nb_cartes_par_joueur=nb_cartes_par_joueur, ui=ui)
+        winner = game.jouer()
+
+        if winner == j1 :
+            j1_wins += 1
+        elif winner == j2 :
+            ia_wins += 1
+        if afficher_score :
+            print(j2, '\n')
+            j1.show_stats(); print()
+            j2.show_stats()
+
+    return j1_wins, ia_wins, n_parties, j1, j2
+
+
 
 ##### Bot Simple vs IA : 400 cartes, 120 tours (27 étages), 4 cartes par joueur, pas d'affichage, 1000 parties
 
@@ -101,6 +126,55 @@ print(f'ia_wins : {ia_wins}/{n_parties}')
 """
 
 
+##### Bot Random vs IA : Graphiques
+
+liste_j1_wins = []
+liste_ia_wins = []
+x = range(5, 28)
+
+for i in x :
+    j1_wins, ia_wins, n_parties, j1, j2 = stats_rand_ia(n_parties=1000, nb_val=int(2+i*(i+1)/8), taille_pyramide=i-1, nb_cartes_par_joueur=4)
+    liste_j1_wins.append(j1_wins)
+    liste_ia_wins.append(ia_wins)
+
+plt.plot(x, liste_j1_wins, label='Bot aléatoire')
+plt.plot(x, liste_ia_wins, label='IA')
+plt.xlabel('Nombre d\'étages dans la pyramide')
+plt.ylabel('Nombre de victoires')
+plt.title('Performances en fonction de la taille de la pyramide\navec un grand deck (400 cartes)')
+plt.legend()
+
+#plt.savefig('figures/perf_rand_ia-taille_pyramide.png')
+plt.show()
+
+
+##### Bot Random vs IA : Graphiques
+
+# 1. si on vide le deck dans la pyramide alors ça change rien qu'il soit grand ou petit (en faisant varier la taille de la main).
+# 2. on peut se demander si laisser bcp de cartes dans la pioche influence les perf de l'ia
+#    ---> etages fixe et pas trop grand, taille de main petit (4) ou grand (20) : resultat ça change rien
+
+etages = 10
+
+liste_j1_wins = []
+liste_ia_wins = []
+x = range(100, 400, 5)
+for i in x :
+    j1_wins, ia_wins, n_parties, j1, j2 = stats_rand_ia(n_parties=500, nb_val=i, taille_pyramide=etages, nb_cartes_par_joueur=20)
+    liste_j1_wins.append(j1_wins)
+    liste_ia_wins.append(ia_wins)
+
+plt.plot(x, liste_j1_wins, label='Bot aléatoire')
+plt.plot(x, liste_ia_wins, label='IA')
+plt.xlabel(f'Nombre de cartes laissées de côté pour une pyramide à {etages} étages')
+plt.ylabel('Nombre de victoires')
+plt.title('Performances en fonction de la quantité de cartes laissées dans le deck')
+plt.legend()
+plt.ylim([0,500])
+
+#plt.savefig('figures/perf_rand_ia-deck-pas_ouf.png')
+plt.show()
+
 
 ##### Bot Simple vs IA : Graphiques
 
@@ -111,67 +185,41 @@ x = range(5, 27)
 for i in x :
 
     j1_wins, ia_wins, n_parties, j1, j2 = stats_bot_ia(n_parties=1000, nb_val=100, taille_pyramide=i, nb_cartes_par_joueur=4)
-
     liste_j1_wins.append(j1_wins)
     liste_ia_wins.append(ia_wins)
 
 #print(f'liste_j1_wins : {liste_j1_wins}')
 #print(f'liste_ia_wins : {liste_ia_wins}')
 
-
-
 plt.plot(x, liste_j1_wins, label='Bot avec stratégie fixe')
 plt.plot(x, liste_ia_wins, label='IA')
 plt.xlabel('Nombre d\'étages dans la pyramide')
 plt.ylabel('Nombre de victoires')
-plt.title('Performances en fonction de la taille de la pyramide\navec un grand deck (400 cartes)')
+plt.title('Performances en fonction de la taille de la pyramide\n(main de 4 cartes)')
 plt.legend()
 
-
-plt.savefig('figures/perf_bot_ia-taille_pyramide.png')
+#plt.savefig('figures/perf_bot_ia-taille_pyramide-petite_main.png')
 plt.show()
 
 
-
-##### Bot Random vs IA : Graphiques
+##### IA vs IA : Graphiques
 
 liste_j1_wins = []
 liste_ia_wins = []
-x = range(5, 27)
+x = range(5, 28)
 
 for i in x :
-    j1_wins, ia_wins, n_parties, j1, j2 = stats_rand_ia(n_parties=1000, nb_val=100, taille_pyramide=i, nb_cartes_par_joueur=4)
+    j1_wins, ia_wins, n_parties, j1, j2 = stats_ia_ia(n_parties=1000, nb_val=int(2+i*(i+1)/8), taille_pyramide=i-1, nb_cartes_par_joueur=4)
     liste_j1_wins.append(j1_wins)
     liste_ia_wins.append(ia_wins)
 
-plt.plot(x, liste_j1_wins, label='Bot aléatoire')
-plt.plot(x, liste_ia_wins, label='IA')
+plt.plot(x, liste_j1_wins, label='IA 1')
+plt.plot(x, liste_ia_wins, label='IA 2')
 plt.xlabel('Nombre d\'étages dans la pyramide')
 plt.ylabel('Nombre de victoires')
-plt.title('Performances en fonction de la taille de la pyramide\navec un grand deck (400 cartes)')
+plt.ylim([0, 1000])
+plt.title('Performances en fonction de la taille de la pyramide\n(main de 4 cartes)')
 plt.legend()
 
-plt.savefig('figures/perf_rand_ia-taille_pyramide.png')
-plt.show()
-
-
-##### Bot Random vs IA : Graphiques
-
-liste_j1_wins = []
-liste_ia_wins = []
-x = range(1,100)
-
-for i in x :
-    j1_wins, ia_wins, n_parties, j1, j2 = stats_rand_ia(n_parties=100, nb_val=100, taille_pyramide=15, nb_cartes_par_joueur=i)
-    liste_j1_wins.append(j1_wins)
-    liste_ia_wins.append(ia_wins)
-
-plt.plot(x, liste_j1_wins, label='Bot aléatoire')
-plt.plot(x, liste_ia_wins, label='IA')
-plt.xlabel('Taille de la main')
-plt.ylabel('Nombre de victoires')
-plt.title('Performances en fonction du nombre de cartes en main')
-plt.legend()
-
-plt.savefig('figures/perf_rand_ia-taille_main.png')
+#plt.savefig('figures/perf_ia_ia-taille_pyramide.png')
 plt.show()
